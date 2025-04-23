aa2fe4243a93a0f85f8cd35d833cef6e2e65ce9c
RozKoy <okkoy.1401@gmail.com>
2025-04-23 21:06:13 +0700

    feat(model n migration): achievement

---
+++ app/Models/Achievement.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Achievement extends Model
+{
+    use HasUuids;
+
+    /*
+    | -----------------------------------------------------------------
+    | Protected Variables
+    | -----------------------------------------------------------------
+    */
+
+    protected $table = 'achievements';
+
+    protected $fillable = [
+        'description',
+        'image',
+        'date',
+        'name',
+    ];
+
+    protected $hidden = [];
+
+    /*
+    | -----------------------------------------------------------------
+    | Public Variables
+    | -----------------------------------------------------------------
+    */
+
+    public const MAX = [
+        'description' => 65535,
+        'image' => 65535,
+        'name' => 255,
+    ];
+
+    /*
+    | -----------------------------------------------------------------
+    | Protected Functions
+    | -----------------------------------------------------------------
+    */
+
+    /**
+     * @return array
+     */
+    protected function casts(): array
+    {
+        return [
+            'date' => 'date',
+        ];
+    }
+}
