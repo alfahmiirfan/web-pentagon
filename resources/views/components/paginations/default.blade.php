8565f8a28730744a2c84f1b435aebddb05fd3c0c
RozKoy <okkoy.1401@gmail.com>
2025-04-26 07:13:36 +0700

    fix(component): render pagination n admin layout for success message

--- resources/views/components/paginations/default.blade.php
+++ resources/views/components/paginations/default.blade.php
@@ -1,17 +1,26 @@
 <div class="flex justify-end gap-1.5">
-    <button x-on:click="currentPage = currentPage > 1 ? currentPage - 1 : 1"
+    <button x-on:click.debounce="currentPage = currentPage > 1 ? currentPage - 1 : 1"
         class="rounded-lg border-2 border-cstm-blue-900 p-2.5 hover:bg-slate-200">
         <img src="/icons/v.svg" alt="" class="w-3 rotate-90">
     </button>
 
     <template x-for="item in maxPage">
-        <button x-on:click="currentPage = item" x-text="item"
-            x-bind:class="(currentPage === item ? 'bg-cstm-blue-900 text-white hover:bg-cstm-blue-900/75' :
-                'hover:bg-slate-200') + ' rounded-lg border-2 border-cstm-blue-900 px-2.5'">
-        </button>
+        <div class="flex">
+            <template x-if="item === 1 || item === maxPage || (item >= currentPage - 1 && item <= currentPage + 1)">
+                <button x-on:click.debounce="currentPage = item" x-text="item"
+                    x-bind:class="(currentPage === item ? 'bg-cstm-blue-900 text-white hover:bg-cstm-blue-900/75' :
+                        'hover:bg-slate-200') + ' rounded-lg border-2 border-cstm-blue-900 px-2.5 flex-1'">
+                </button>
+            </template>
+            <template x-if="item > 1 && item < maxPage && (item === currentPage - 2 || item === currentPage + 2)">
+                <p class="flex-1 rounded-lg border-2 border-cstm-blue-900 px-2.5">
+                    ...
+                </p>
+            </template>
+        </div>
     </template>
 
-    <button x-on:click="currentPage = currentPage < maxPage ? currentPage + 1 : maxPage"
+    <button x-on:click.debounce="currentPage = currentPage < maxPage ? currentPage + 1 : maxPage"
         class="rounded-lg border-2 border-cstm-blue-900 p-2.5 hover:bg-slate-200">
         <img src="/icons/v.svg" alt="" class="w-3 -rotate-90">
     </button>
