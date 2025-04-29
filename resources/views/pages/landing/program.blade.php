7f8ad815bde47dda77d067a88f6ca5e953363179
RozKoy <okkoy.1401@gmail.com>
2025-04-29 08:39:53 +0700

    feat(program - landing): responsive

--- resources/views/pages/landing/program.blade.php
+++ resources/views/pages/landing/program.blade.php
@@ -3,29 +3,31 @@
     <div class="flex justify-center bg-cstm-green-50">
         <div
             class="flex w-full max-w-screen-xl flex-col items-center justify-center gap-2.5 px-5 pb-12 pt-36 2xl:max-w-screen-2xl">
-            <h1 title="Program Unggulan" class="w-full text-4xl font-bold">
+            <h1 title="Program Unggulan" class="w-full text-2xl font-bold sm:text-3xl lg:text-4xl">
                 Program Unggulan
             </h1>
-            <h1 title="SMA Negeri 10 Kaur Pentagon" class="w-full text-4xl font-bold">
+            <h1 title="SMA Negeri 10 Kaur Pentagon" class="w-full text-2xl font-bold sm:text-3xl lg:text-4xl">
                 SMA Negeri 10 Kaur Pentagon
             </h1>
-            <p title="Bergabunglah Bersama Kami di Tahun Ajaran Baru!" class="w-full text-xl text-gray-500">
+            <p title="Bergabunglah Bersama Kami di Tahun Ajaran Baru!" class="w-full text-gray-500 sm:text-lg lg:text-xl">
                 Bergabunglah Bersama Kami di Tahun Ajaran Baru!
             </p>
         </div>
     </div>
 
     <div class="flex min-h-screen flex-col items-center justify-center">
-        <div class="flex w-full max-w-screen-xl items-center justify-between gap-10 px-5 py-10 2xl:max-w-screen-2xl">
-            <div class="flex flex-1 items-center justify-start">
-                <img src="/images/landing/nanotechnology.jpg" alt="Nanotechnology" class="w-5/6 rounded-lg">
+        <div id="nanotechnology"
+            class="flex w-full max-w-screen-xl flex-wrap items-center justify-between gap-6 px-5 py-10 md:gap-10 2xl:max-w-screen-2xl">
+            <div class="flex items-center justify-center max-md:w-full md:flex-1 md:justify-start">
+                <img src="/images/landing/nanotechnology.jpg" alt="Nanotechnology" class="w-full rounded-lg sm:w-5/6">
             </div>
-            <div class="flex flex-1 flex-col items-start justify-center gap-5">
+            <div class="flex flex-col items-start justify-center gap-5 max-md:w-full max-md:text-sm md:flex-1">
                 <div class="h-1 w-20 bg-cstm-green-900"></div>
-                <h3 class="text-3xl font-bold">
+                <h3 title="Nanotechnology" class="text-xl font-bold sm:text-2xl lg:text-3xl">
                     Nanotechnology
                 </h3>
-                <p class="text-gray-500">
+                <p title="Program ini mengenalkan siswa pada dunia material berskala nano, teknologi canggih yang menjadi fondasi masa depan industri dan kesehatan. Siswa akan diajak memahami struktur, aplikasi, serta eksperimen dasar nanotechnology."
+                    class="text-gray-500">
                     Program ini mengenalkan siswa pada dunia material berskala nano, teknologi canggih yang menjadi
                     fondasi masa depan industri dan kesehatan. Siswa akan diajak memahami struktur, aplikasi, serta
                     eksperimen dasar nanotechnology.
@@ -43,17 +45,18 @@
                 </ul>
             </div>
         </div>
-        <div
-            class="flex w-full max-w-screen-xl flex-row-reverse items-center justify-between gap-10 px-5 py-10 2xl:max-w-screen-2xl">
-            <div class="flex flex-1 items-center justify-end">
-                <img src="/images/landing/biotechnology.jpg" alt="Biotechnology" class="w-5/6 rounded-lg">
+        <div id="biotechnology"
+            class="flex w-full max-w-screen-xl flex-wrap items-center justify-between gap-6 px-5 py-10 md:flex-row-reverse md:gap-10 2xl:max-w-screen-2xl">
+            <div class="flex items-center justify-center max-md:w-full md:flex-1 md:justify-end">
+                <img src="/images/landing/biotechnology.jpg" alt="Biotechnology" class="w-full rounded-lg sm:w-5/6">
             </div>
-            <div class="flex flex-1 flex-col items-start justify-center gap-5">
+            <div class="flex flex-col items-start justify-center gap-5 max-md:w-full max-md:text-sm md:flex-1">
                 <div class="h-1 w-20 bg-cstm-green-900"></div>
-                <h3 class="text-3xl font-bold">
+                <h3 title="Biotechnology" class="text-xl font-bold sm:text-2xl lg:text-3xl">
                     Biotechnology
                 </h3>
-                <p class="text-gray-500">
+                <p title="Bioteknologi adalah jembatan antara biologi dan teknologi. Program ini memberikan pemahaman tentang bagaimana organisme hidup digunakan untuk menciptakan produk atau layanan bermanfaat di bidang pertanian, kesehatan, dan lingkungan."
+                    class="text-gray-500">
                     Bioteknologi adalah jembatan antara biologi dan teknologi. Program ini memberikan pemahaman tentang
                     bagaimana organisme hidup digunakan untuk menciptakan produk atau layanan bermanfaat di bidang
                     pertanian, kesehatan, dan lingkungan.
@@ -71,16 +74,18 @@
                 </ul>
             </div>
         </div>
-        <div class="flex w-full max-w-screen-xl items-center justify-between gap-10 px-5 py-10 2xl:max-w-screen-2xl">
-            <div class="flex flex-1 items-center justify-start">
-                <img src="/images/landing/robotic.jpg" alt="Robotic" class="w-5/6 rounded-lg">
+        <div id="robotic"
+            class="flex w-full max-w-screen-xl flex-wrap items-center justify-between gap-6 px-5 py-10 md:gap-10 2xl:max-w-screen-2xl">
+            <div class="flex items-center justify-center max-md:w-full md:flex-1 md:justify-start">
+                <img src="/images/landing/robotic.jpg" alt="Robotic" class="w-full rounded-lg sm:w-5/6">
             </div>
-            <div class="flex flex-1 flex-col items-start justify-center gap-5">
+            <div class="flex flex-col items-start justify-center gap-5 max-md:w-full max-md:text-sm md:flex-1">
                 <div class="h-1 w-20 bg-cstm-green-900"></div>
-                <h3 class="text-3xl font-bold">
+                <h3 title="Robotic" class="text-xl font-bold sm:text-2xl lg:text-3xl">
                     Robotic
                 </h3>
-                <p class="text-gray-500">
+                <p title="Membentuk generasi yang melek teknologi dan siap bersaing dalam dunia industri 4.0 melalui pemrograman dasar, perakitan robot, dan kompetisi antar siswa. Robotic juga menanamkan logika berpikir, problem solving, dan teamwork."
+                    class="text-gray-500">
                     Membentuk generasi yang melek teknologi dan siap bersaing dalam dunia industri 4.0 melalui
                     pemrograman dasar, perakitan robot, dan kompetisi antar siswa. Robotic juga menanamkan logika
                     berpikir, problem solving, dan teamwork.
@@ -98,17 +103,18 @@
                 </ul>
             </div>
         </div>
-        <div
-            class="flex w-full max-w-screen-xl flex-row-reverse items-center justify-between gap-10 px-5 py-10 2xl:max-w-screen-2xl">
-            <div class="flex flex-1 items-center justify-end">
-                <img src="/images/landing/energy.jpg" alt="Energy" class="w-5/6 rounded-lg">
+        <div id="energy"
+            class="flex w-full max-w-screen-xl flex-wrap items-center justify-between gap-6 px-5 py-10 md:flex-row-reverse md:gap-10 2xl:max-w-screen-2xl">
+            <div class="flex items-center justify-center max-md:w-full md:flex-1 md:justify-end">
+                <img src="/images/landing/energy.jpg" alt="Energy" class="w-full rounded-lg sm:w-5/6">
             </div>
-            <div class="flex flex-1 flex-col items-start justify-center gap-5">
+            <div class="flex flex-col items-start justify-center gap-5 max-md:w-full max-md:text-sm md:flex-1">
                 <div class="h-1 w-20 bg-cstm-green-900"></div>
-                <h3 class="text-3xl font-bold">
+                <h3 title="Renewable Energy" class="text-xl font-bold sm:text-2xl lg:text-3xl">
                     Renewable Energy
                 </h3>
-                <p class="text-gray-500">
+                <p title="Mempersiapkan siswa untuk peduli dan aktif dalam solusi energi masa depan. Program ini memperkenalkan jenis-jenis energi terbarukan seperti surya, angin, dan biomassa, serta cara pemanfaatannya dalam kehidupan sehari-hari."
+                    class="text-gray-500">
                     Mempersiapkan siswa untuk peduli dan aktif dalam solusi energi masa depan. Program ini
                     memperkenalkan jenis-jenis energi terbarukan seperti surya, angin, dan biomassa, serta cara
                     pemanfaatannya dalam kehidupan sehari-hari.
