2ab495fb2024a1164e32f0feb0a48ce872b789e8
RozKoy <okkoy.1401@gmail.com>
2025-05-12 15:01:04 +0700

    enhance(home n detail information - landing): layouting n description render

--- resources/views/pages/landing/achievement/detail.blade.php
+++ resources/views/pages/landing/achievement/detail.blade.php
@@ -3,8 +3,9 @@
     <div class="flex flex-col items-center justify-center">
         <div
             class="flex w-full max-w-screen-xl flex-col gap-3 px-5 pb-10 pt-32 text-justify max-sm:text-sm md:pt-40 2xl:max-w-screen-2xl">
-            <div class="aspect-[2/1] w-full rounded-lg border bg-cover bg-center bg-no-repeat"
-                style="background-image: url('/storage/{{ $achievement->image }}')">
+            <div class="flex aspect-[2/1] w-full items-center justify-center rounded-lg border-2 p-1.5">
+                <img src="/storage/{{ $achievement->image }}" alt=""
+                    class="mx-auto aspect-auto h-full max-h-screen max-w-full rounded-lg">
             </div>
             <h6 title="{{ $achievement->name }}" class="text-xl font-bold sm:text-2xl md:text-3xl">
                 {{ $achievement->name }}
